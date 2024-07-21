ebb6070b7a0f959cf12fcaf7a8aa0635f4316cbd
Matt Gerig <mattgerig@gmail.com>
2024-07-21 16:44:17 -0500

    HG: Now using expo router and removing nativewind. Using base react native stylesheets. Created home page. Next is component library

---
+++ app/(pages)/_layout.tsx
@@ -0,0 +1,37 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "(tabs)",
+};
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="*" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
